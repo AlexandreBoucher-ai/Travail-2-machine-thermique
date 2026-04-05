b4d22ba57da5ba4f7fb0ce2a5d56eef4864e4dc6
Alexandre Boucher <albou631@ulaval.ca>
2026-04-05 05:03:46 -0400

    fisrt

---
+++ main.py
@@ -0,0 +1,7 @@
+import CoolProp.CoolProp as CP
+
+# CP.PropsSI(propriétés, entré1, valeur1, entré2, valeur2, R410A)
+
+# fonxrion prend température en entré (ce qu'on va tester)
+def a(T):
+    CP.PropsSI(propriétés, entré1, valeur1, entré2, valeur2, R410A)
