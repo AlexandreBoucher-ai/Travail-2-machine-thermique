1e5e7e5d615dc82d2d3c3b527fd96a20674f25ee
Alexandre Boucher <albou631@ulaval.ca>
2026-04-05 06:49:23 -0400

    fonction a est correct

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import CoolProp.CoolProp as CP
+import numpy as np
 
 # Remarque Q = titre, donc si Q = 1 vapeur sat et si Q =0 liquide sat
 
@@ -10,14 +11,14 @@ TL = 21 + 273.15 # (fixé)
 # TEMPÉRATURE EN KELVIN
 def a(TH):
     # État 1:
-    T1 = TH - (8 + 273.15)
+    T1 = TH - 8
     h1 = CP.PropsSI('H', 'T', T1, 'Q', 1, 'R410A')
     s1 = CP.PropsSI('S', 'T', T1, 'Q', 1, 'R410A')
     D1 = CP.PropsSI('D', 'T', T1, 'Q', 1, 'R410A')
     P1 = CP.PropsSI('P', 'T', T1, 'Q', 1, 'R410A')
     m = Débitcompresseur * D1 # rép
     # État 3:
-    T3 = TL + (20 + 273.15)
+    T3 = TL + 20
     h3 = CP.PropsSI('H', 'T', T3, 'Q', 0, 'R410A')
     s3 = CP.PropsSI('S', 'T', T3, 'Q', 0, 'R410A')
     D3 = CP.PropsSI('D', 'T', T3, 'Q', 0, 'R410A')
@@ -26,20 +27,24 @@ def a(TH):
     s2 = s1
     P2 = P3
     h2s = CP.PropsSI('H', 'S', s2, 'P', P2, 'R410A')
-    h2 = 0.85(h2s-h1) + h1
+    h2 = 0.85 * (h2s-h1) + h1
     # État 4
     P4 = P1
     s4 = s3
-    h4 = CP.PropsSI('H', 'S', s2, 'P', P2, 'R410A')
+    h4 = CP.PropsSI('H', 'S', s4, 'P', P4, 'R410A')
     # QH
-    QH = m * (h3 - h2) # rép
+    QH = m * (h2 - h3) # rép
     # wcomp
     wcomp = h2 - h1 # rép
     # Wcomp
-    Wcomp = m(h2 - h1) # rép
+    Wcomp = m * (h2 - h1) # rép
     # COP
     COP = QH / Wcomp # rép
     return (m, QH, wcomp, Wcomp, COP)
+
+TH = arange(243.15, 288.15 + 1, 5)
+for i in TH:
+    print(a(i))
 
 
 
